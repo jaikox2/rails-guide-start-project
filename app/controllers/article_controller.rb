20827d8cf4a77aa83de881f2d3832125bcc8a7a4
pang dev <pangdev58@gmail.com>
2021-04-30 11:38:14 +0700

    implement rails Model to rails Controller

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,4 +1,9 @@
 class ArticleController < ApplicationController
   def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "article#index"
 
-  get "/article", to: "article#index"
+  get "/articles", to: "article#index"
+  get "/articles/:id", to: "article#show"
 end
